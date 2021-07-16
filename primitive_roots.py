aad9f5cf8d20569b274d7f352faa7ef164b770be
Simon Lee <76032245+Simonlee711@users.noreply.github.com>
2021-07-16 11:40:19 -0700

    Update primitive_roots.py

--- primitive_roots.py
+++ primitive_roots.py
@@ -2,8 +2,7 @@
 Module that will help find primtive roots of a number given certain criteria
 
 Definition of Primitve Root:
-That is, the integer g is a primitive root (mod n) if for every number aa relatively prime to n there is an integer z such that a \equiv \big(g^z \pmod{n}\big).a≡(g 
-z (modn)).
+That is, the integer g is a primitive root (mod n) if for every number aa relatively prime to n there is an integer z such that a ≡ (g^z (mod n)).
 """
 __author__ = 'Simon Lee in Math 134, siaulee@ucsc.edu'
 
